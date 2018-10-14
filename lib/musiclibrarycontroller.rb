15019a6006ec5e7dd70226a3adc5c9e60c880074
Rachel Grier <rgrier104@gmail.com>
2018-10-14 19:49:52 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -22,6 +22,16 @@ class MusicLibraryController
       input = gets.strip
       if input == "list songs"
         list_songs
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres
+      elsif input == "list artist"
+        list_songs_by_artist
+      elsif input == "list genre"
+        list_songs_by_genre
+      elsif play_song
+        play_song
       end
       input = gets.strip
     end
